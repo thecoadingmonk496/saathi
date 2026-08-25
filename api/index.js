256448b8a28a4487e916015ab09d4cfe6d29dc98
thecoadingmonk496 <ts7529614@gmail.com>
2026-08-25 22:44:58 +0530

    Fix Vercel serverless function crash by removing deleted blockchainRoutes

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('../backend/config/db');
 const authRoutes = require('../backend/routes/auth');
-const blockchainRoutes = require('../backend/routes/blockchain');
 const adminRoutes = require('../backend/routes/admin');
 const mandiRoutes = require('../backend/routes/mandi');
 const priceHistoryRoutes = require('../backend/routes/priceHistory');
@@ -35,7 +34,6 @@ app.use(async (req, res, next) => {
 
 // Mounted Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/blockchain', blockchainRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/mandi-prices', mandiRoutes);
 app.use('/api/price-history', priceHistoryRoutes);
